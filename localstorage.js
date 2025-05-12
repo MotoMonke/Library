e0ae954cc249dd946a935da32026dc4c70456d29
Daniil <developmentweb15@gmail.com>
2025-05-12 20:33:04 +0300

    Added local storage

--- localstorage.js
+++
@@ -1,7 +0,0 @@
-function saveProjectArray(){
-    localStorage.setItem("Books",JSON.stringify(projectsArray));
-}
-function loadProjectArray() {
-    const data = localStorage.getItem("Books");
-    return data ? JSON.parse(data) : [];
-}
